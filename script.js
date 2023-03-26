dd940b087ceb85f42758c29997e5b3a2cc135a01
gilsomarriba <115040779+gilsomarriba@users.noreply.github.com>
2023-03-26 16:33:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+function jsFormacao() {
+    var x = document.getElementById("js-formacao");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function jsExpeciencias() {
+    var x = document.getElementById("js-experiencias");
+    if (x.style.display === "none") {
+      x.style.display = "grid";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  
+  function jsConhecimentos() {
+    var x = document.getElementById("js-conhecimentos");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function jsLinguagens() {
+    var x = document.getElementById("js-linguagens");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function jsCursos() {
+    var x = document.getElementById("js-cursos");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  
+  
+   function jsRealizacoes() {
+    var x = document.getElementById("js-realizacoes");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+
+
+
+
+
